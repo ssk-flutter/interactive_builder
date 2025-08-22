50d85c41e7fdf5a24745055321cb8a801beee97b
ssk <developer.kss@gmail.com>
2025-08-22 15:53:29 +0900

    feat: update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -43,44 +43,44 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: _toggleActive,
-            child: Text('Toggle Active State: ${_isActive ? 'Activated' : 'Deactivated'}'),
-          ),
-          const SizedBox(height: 20),
-          Text('Counter: $_counter', style: Theme.of(context).textTheme.headlineMedium),
-          const SizedBox(height: 20),
-          InteractiveBuilder(
-            onTap: _isActive ? () => setState(() => _counter = 0) : null,
-            builder: (context, state, child) => Text(
-              'Clear',
-              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                color: state.resolve(
-                  Colors.blue[300],
-                  hovered: Colors.blue[600]!,
-                  pressed: Colors.blue[900]!,
-                  deactivated: Colors.grey,
+      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _toggleActive,
+              child: Text('Toggle Active State: ${_isActive ? 'Activated' : 'Deactivated'}'),
+            ),
+            const SizedBox(height: 20),
+            Text('Counter: $_counter', style: Theme.of(context).textTheme.headlineMedium),
+            const SizedBox(height: 20),
+            InteractiveBuilder(
+              onTap: _isActive ? () => setState(() => _counter = 0) : null,
+              builder: (context, state, child) => Text(
+                'Clear',
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                  color: state.resolve(
+                    Colors.blue[300],
+                    hovered: Colors.blue[600]!,
+                    pressed: Colors.blue[900]!,
+                    deactivated: Colors.grey,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          InteractiveBuilder(
-            onTap: _isActive ? _incrementCounter : null,
-            builder: (context, state, child) => AnimatedScale(
-              scale: state.resolve(1.0, pressed: 1.0, hovered: 1.1),
-              duration: const Duration(milliseconds: 200),
-              child: Padding(padding: const EdgeInsets.all(8.0), child: const Icon(Icons.add)),
+            const SizedBox(height: 20),
+            InteractiveBuilder(
+              onTap: _isActive ? _incrementCounter : null,
+              builder: (context, state, child) => AnimatedScale(
+                scale: state.resolve(1.0, pressed: 1.0, hovered: 1.1),
+                duration: const Duration(milliseconds: 200),
+                child: Padding(padding: const EdgeInsets.all(8.0), child: const Icon(Icons.add)),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
+    );
 }
